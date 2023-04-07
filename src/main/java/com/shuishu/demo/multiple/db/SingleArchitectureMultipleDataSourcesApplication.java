9670790352d4a738c4159099ad70ee319c114c19
谁书-ss <yushuishu@qq.com>
2023-04-07 12:39:42 +0800

    feat:单体架构多数据源

---
+++ src/main/java/com/shuishu/demo/multiple/db/SingleArchitectureMultipleDataSourcesApplication.java
@@ -0,0 +1,22 @@
+package com.shuishu.demo.multiple.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author ：谁书-ss
+ * @date   ： 2023-04-07 12:37
+ * @IDE    ：IntelliJ IDEA
+ * @Motto  ：ABC(Always Be Coding)
+ * <p></p>
+ * @description ：单体架构，多数据源
+ */
+@SpringBootApplication
+public class SingleArchitectureMultipleDataSourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleArchitectureMultipleDataSourcesApplication.class, args);
+    }
+
+}
